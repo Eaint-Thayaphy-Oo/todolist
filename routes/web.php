09378873259f69ee2fa7c99dc31395e05def85c4
Eaint-Thayaphy-Oo <eaintthayaphyoo098@gmail.com>
2023-08-17 15:52:50 +0630

    Routes commit

--- routes/web.php
+++ routes/web.php
@@ -14,29 +14,20 @@ use App\Http\Controllers\StudentController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+//Routes for PostCRUD
 Route::get('/', [PostController::class, 'create'])->name('post#home');
-
 Route::get('customer/createPage', [PostController::class, 'create'])->name('post#createPage');
-
 Route::post('post/create', [PostController::class, 'postCreate'])->name('post#create');
-
 Route::get('testing', function () {
     return "this is testing...";
 })->name('test');
-
 // Route::get('post/delete/{id}', [PostController::class,'postDelete'])->name('post#delete');
-
 Route::delete('post/delete/{id}', [PostController::class, 'postDelete'])->name('post#delete');
-
 Route::get('post/updatePage/{id}', [PostController::class, 'updatePage'])->name('post#updatePage');
-
 Route::get('post/editPage/{id}', [PostController::class, 'editPage'])->name('post#editPage');
-
 Route::post('post/update', [PostController::class, 'update'])->name('post#update');
 
+//Routes for StudentCRUD
 Route::get('/', [StudentController::class, 'home'])->name('student#home');
-
 Route::get('student/createPage',[StudentController::class,'create'])->name('student#createPage');
-
 Route::post('student/create', [StudentController::class,'studentCreate'])->name('student#create');
